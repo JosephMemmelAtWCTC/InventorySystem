929187d90e776408c3fcc87eb202533599fa05e5
42null <averyparanoiduser@gmail.com>
2024-02-09 00:42:22 -0600

    Mobile footer navigation

--- AddComponents.js
+++ AddComponents.js
@@ -4,7 +4,7 @@ $(function(){
     // https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_ajax_load
     // $("#div1").replaceWith($('<div id="remove">').load("./components/MainFooter.html"));
 
-    $.get("./components/MainFooter.html", function(data) {
-        $("#div1").replaceWith(data);
-    });
+    // $.get("./components/MainFooter.html", function(data) {
+    //     $("#div1").replaceWith(data);
+    // });
 });
