1aeb7243517ce48aa0e5e52df3c58aa50b505e6e
42null <averyparanoiduser@gmail.com>
2024-02-11 21:17:17 -0600

    Models + js nav + add &+

---
+++ InventorySystem.js
@@ -0,0 +1,35 @@
+$(function(){
+    $pages = $(".page");
+
+    $('.primaryNavMovePage').on('click', function (e){
+        const $this = $(this);
+        const openPageTarget = $this.attr("data-navPageTarget");
+        $pages.each( (index, page) => {
+            const $page = $(page);
+            if($page.attr("data-navPage") === openPageTarget) {
+                $page.removeClass("d-none");
+            }else{
+                $page.addClass("d-none");
+            }
+        });
+    });
+
+
+    const $model = $('.model');
+    $model.on('show.bs.modal', event => {
+        // Button that triggered the modal
+        const button = event.relatedTarget
+        // Extract info from data-bs-* attributes
+        const recipient = button.getAttribute('data-bs-whatever')
+        // If necessary, you could initiate an Ajax request here
+        // and then do the updating in a callback.
+
+        // Update the modal's content.
+        const modalTitle = exampleModal.querySelector('.modal-title')
+        const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+        modalTitle.textContent = `New message to ${recipient}`
+        modalBodyInput.value = recipient
+    })
+
+});
