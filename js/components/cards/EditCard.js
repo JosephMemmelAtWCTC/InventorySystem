e8b382f2fc7ffa409e5c9bce0690fca0d40c687f
42null <averyparanoiduser@gmail.com>
2024-04-01 01:02:24 -0500

    First category edit fields appearance

--- js/components/cards/EditCard.js
+++ js/components/cards/EditCard.js
@@ -1,28 +1,59 @@
 app.component('EditCard', {
     data() {
         return {
-            uid: 'sli-' + Math.floor(Math.random() * 10e15),
+            editItem: null,
         }
     },
     props: {
+        cardComponent: {
+            type: String,
+            required: true,
+        },
         item: {
             type: Object,
             required: true,
         },
-    },
-    emits: ['card-clicked'],
-    methods: {
-        sendUpdateCardClicked() {
-            if(this.item){
-                this.$emit('card-clicked', this.item);
-            }else{
-                this.$emit('card-clicked');
-            }
+        editItem: {
+            type: Object,
+            required: true,
         }
     },
+    emits: [],
+    methods: {
+        openEditModal(){
+            this.$refs.editModal.openModal();
+        },
+    },
     template: `
-        <template #card @click="">
-        </template>
+<!--TODO: Fix @card-clicked="openEditModal" over @click-->
+<!--TODO: Put back item/category string in modal parts-->
+        <component :is="cardComponent" :item="item" @click="openEditModal">
+        </component>
+        <edit-modal :item="this.editItem"
+                    @remove-it="removeItem"
+                    can-remove
+                    title="Edit ___"
+                    submit-button-text="Update ___"
+                    ref="editModal"
+        >
+            <template v-slot="slotProps">
+                <q-input filled v-model="slotProps.editItem.title"
+                         autofocus
+                         label="Name"
+                         class="full-width"
+                         :rules="[val => !!val || '* Required']"
+                         lazy-rules
+                ></q-input>
+                <q-input filled v-model="slotProps.editItem.description"
+                         type="textarea"
+                         rows="4"
+                         label="Description"
+                         class="full-width"
+                         :rules="[val => !!val || '* Required']"
+                         lazy-rules
+                ></q-input>
+            </template>
+        </edit-modal>
     `,
 
 });
--- js/components/cards/EditCategoryCard.js
+++ js/components/cards/EditCategoryCard.js
@@ -1,42 +1,29 @@
 app.component('EditCategoryCard', {
     data() {
         return {
-            uid: 'sli-' + Math.floor(Math.random() * 10e15),
-            editCategory: null,
+            editItem: null,
         }
     },
     props: {
         item: {
             type: Object,
             required: true,
+        },
+        editItem: {
+            type: Object,
+            required: true,
         }
     },
-    emits: ['card-clicked'],
+    emits: [],
     methods: {
-        openEditModal(){
-            console.log("openEditModalopenEditModalopenEditModalopenEditModalopenEditModalopenEditModalopenEditModalopenEditModal");
-            this.$refs.editModal.openModal();
-        },
-        sendUpdateCardClicked() {
-            if(this.item){
-                this.$emit('card-clicked', this.item);
-            }else{
-                this.$emit('card-clicked');
-            }
-        },
     },
     template: `
-<!--TODO: Fix @card-clicked="openEditModal" over @click-->
-        <category-card :item="item" @click="openEditModal">
-        </category-card>
-<!--        <template #extra v-slot="slotProps">-->
-        <edit-modal :item="this.editItem"
-                    @remove-it="removeItem"
-                    can-remove
-                    title="Edit Item"
-                    :submit-button-text="'Update Item'"
-                    ref="editModal"
-        ></edit-modal>
+        <edit-card card-component="CategoryCard" :item="item">
+        A_ISJKSNLSKNS
+            <template #form>
+                ISJKSNLSKNS
+            </template>
+        </edit-card>
     `,
 
 });
